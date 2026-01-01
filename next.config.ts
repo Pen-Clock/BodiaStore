57a52a434311553d48ddd5d444e286c03917ff58
Ratanak Oudom Samnang <z5547618@ad.unsw.edu.au>
2026-01-01 19:15:45 +1100

    add config to make uploadThing work

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '1m5113h8dg.ufs.sh',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
